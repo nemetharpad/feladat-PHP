544db371bfcb552cac5c15dbc68193c096f3d464
Arpi <arpimail123@gmail.com>
2021-05-03 19:24:10 +0200

    aTargy20alatt added

--- index.php
+++ index.php
@@ -1,4 +1,38 @@
 <?php
 
+require_once('homework_input.php');
 
-?>
+class Felvetelizo {
+
+    public function __construct($adatok){
+        $this->adatok = $adatok;
+    }
+
+
+
+    private function aTargy20alatt() {
+        foreach ($this->adatok['erettsegi-eredmenyek'] as $value) {
+            if(rtrim($value['eredmeny'], "%") < 20){
+                return $value['nev'];
+            }          
+        }
+    }
+
+    public function pontszamitas(){
+        if($this -> aTargy20alatt()){
+            echo "hiba, nem lehetséges a pontszámítás a ". $this -> aTargy20alatt() ." tárgyból elért 20% alatti eredmény miatt";
+            return;
+        }
+
+
+    }
+}
+$felvetelizo = new Felvetelizo($exampleData3);
+//echo $felvetelizo->adatok;
+$felvetelizo->pontszamitas();
+
+
+
+
+
+
