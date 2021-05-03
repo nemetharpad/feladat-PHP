10433b0435a6a108510409053ccb2b327cbdcb78
Arpi <arpimail123@gmail.com>
2021-05-03 20:13:59 +0200

    hianyzikKotelezoTargy

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ class Felvetelizo {
         $this->adatok = $adatok;
     }
 
-
+    //private $kotelezoTargyak = ["magyar nyelv és irodalom", "történelem", "matematika"];
 
     private function aTargy20alatt() {
         foreach ($this->adatok['erettsegi-eredmenyek'] as $value) {
@@ -18,20 +18,35 @@ class Felvetelizo {
         }
     }
 
+    private function hianyzikKotelezoTargy() {
+        $kotelezoTargyak = ["magyar nyelv és irodalom", "történelem", "matematika"];
+        $tmp = [];
+        foreach ($this->adatok['erettsegi-eredmenyek'] as $value) {
+            array_push($tmp, $value['nev']);     
+        }
+        return array_diff($kotelezoTargyak, $tmp);
+    }
+
+
+
     public function pontszamitas(){
         if($this -> aTargy20alatt()){
             echo "hiba, nem lehetséges a pontszámítás a ". $this -> aTargy20alatt() ." tárgyból elért 20% alatti eredmény miatt";
             return;
         }
 
+        if($this -> hianyzikKotelezoTargy()){
+            echo "hiba, nem lehetséges a pontszámítás a kötelező érettségi tárgyak hiánya miatt";
+            return;
+        }
+
 
     }
 }
-$felvetelizo = new Felvetelizo($exampleData3);
-//echo $felvetelizo->adatok;
+
+$felvetelizo = new Felvetelizo($exampleData2);
+
 $felvetelizo->pontszamitas();
-
-
 
 
 
